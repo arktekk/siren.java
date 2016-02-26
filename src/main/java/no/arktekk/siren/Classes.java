762b105e789d93b16fcd528461407e27ec3cbb00
Erlend Hamnaberg <erlend@hamnaberg.net>
2016-02-26 22:54:18 +0100

    More convenience stuff.

--- src/main/java/no/arktekk/siren/Classes.java
+++ src/main/java/no/arktekk/siren/Classes.java
@@ -1,9 +1,11 @@
 package no.arktekk.siren;
 
+import no.arktekk.siren.util.CollectionUtils;
 import no.arktekk.siren.util.StreamUtils;
 import no.arktekk.siren.util.StreamableIterable;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -15,21 +17,44 @@ public final class Classes implements StreamableIterable<String> {
 
     private final List<String> classes;
 
+    private Classes(List<String> classes) {
+        this.classes = unmodifiableList(classes);
+    }
+
     public Classes(Iterable<String> classes) {
-        this.classes = unmodifiableList(StreamUtils.stream(classes).collect(Collectors.toList()));
+        this(StreamUtils.stream(classes).collect(Collectors.toList()));
     }
 
     public static Classes of(String clazz, String... classes) {
-        return new Classes(new ArrayList<String>() {{
-            add(clazz);
-            addAll(asList(classes));
-        }});
+        return new Classes(CollectionUtils.asList(clazz, classes));
+    }
+
+    public static Classes empty() {
+        return new Classes(Collections.emptyList());
     }
 
     public boolean includes(Classes rel) {
         List<String> other = rel.classes;
         List<String> us = this.classes;
         return other.size() <= us.size() && StreamUtils.zip(us.stream(), other.stream(), String::equals).allMatch(t -> t);
+    }
+
+    public Classes add(String clazz) {
+        List<String> list = new ArrayList<>(classes);
+        list.add(clazz);
+        return new Classes(list);
+    }
+
+    public Classes addAll(Classes classes) {
+        List<String> list = new ArrayList<>(this.classes);
+        list.addAll(classes.classes);
+        return new Classes(list);
+    }
+
+    public Classes remove(String clazz) {
+        List<String> list = new ArrayList<>(classes);
+        list.remove(clazz);
+        return new Classes(list);
     }
 
     public Iterator<String> iterator() {
